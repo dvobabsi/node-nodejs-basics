0a0ea281f2fd8f94f8eeec2fa61d1cfef5871a23
dvobabsi <dvobabsi@gmail.com>
2024-10-07 21:23:00 +0300

    feat: add solution fs: delete.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -7,19 +7,16 @@ const rename = async () => {
     const properFilename = path.resolve('src', 'fs', 'files', 'properFilename.md');
 
     try {
-        // Проверяем существование wrongFilename.txt
+
         await fs.access(wrongFilename, constants.F_OK);
-        // Проверяем, существует ли уже properFilename.md
         try {
             await fs.access(properFilename, constants.F_OK);
-            throw new Error('FS operation failed'); // Если properFilename.md существует, выбрасываем ошибку
+            throw new Error('FS operation failed');
         } catch {
-            // Если properFilename.md не существует, выполняем переименование
             await fs.rename(wrongFilename, properFilename);
             console.log(`Renamed ${wrongFilename} to ${properFilename}`);
         }
     } catch (err) {
-        // Обрабатываем ошибку, если wrongFilename.txt не существует
         process.stderr.write('FS operation failed\n');
     }
 };
