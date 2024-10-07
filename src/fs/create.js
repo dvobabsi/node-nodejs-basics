5595b57d93c515545896a7cf4ec769640a507752
dvobabsi <dvobabsi@gmail.com>
2024-10-07 20:22:46 +0300

    feat: add solution fs: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
 const create = async () => {
-    // Write your code here 
+    const filePath = join('src','fs','files', 'fresh.txt');
+    const content = 'I am fresh and young';
+    const error = 'FS operation failed';
+
+    try {
+        await fs.access(filePath);
+        throw new Error(error);
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            await fs.writeFile(filePath, content, 'utf8');
+            console.log('File created successfully');
+        } else {
+            process.stderr.write(`Error: ${err.message}\n`);
+            process.exit();
+        }
+    }
 };
 
 await create();
